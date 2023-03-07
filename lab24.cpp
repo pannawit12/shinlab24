0a0ff986cb75ef82f9f4115ba32d77dd8458706a
Your Name <you@example.com>
2023-03-07 15:55:19 +0700

    main

--- lab24.cpp
+++ lab24.cpp
@@ -91,3 +91,43 @@ ostream & operator<<(ostream &os, const ComplexNumber &c){
 	}
 }
 
+int main(){
+	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
+	cout << a << "\n";
+	cout << b << "\n";
+	cout << c << "\n";
+	cout << a+2.5 << "\n";
+	cout << 2.5+a << "\n";
+	cout << a-1.5 << "\n";
+	cout << 1.5-a << "\n";
+	cout << b+ComplexNumber(0,2.5) << "\n";
+	cout << c-c << "\n";
+	cout << "-----------------------------------\n";
+	
+	ComplexNumber d = (a+b)/c;
+	ComplexNumber e = b/(a-c);
+	cout << d << "\n";
+	cout << e << "\n";
+	cout << c*2 << "\n";
+	cout << 0.5*c << "\n";
+	cout << 1/c << "\n";
+	cout << "-----------------------------------\n";
+	
+	cout << ComplexNumber(1,1).abs() << "\n";
+	cout << ComplexNumber(-1,1).abs() << "\n";
+	cout << ComplexNumber(1.5,2.4).abs() << "\n";
+	cout << ComplexNumber(3,4).abs() << "\n";
+	cout << ComplexNumber(69,-9).abs() << "\n";		
+	cout << "-----------------------------------\n";	
+	
+	cout << ComplexNumber(1,1).angle() << "\n";
+	cout << ComplexNumber(-1,1).angle() << "\n";
+	cout << ComplexNumber(-1,-1).angle() << "\n";
+	cout << ComplexNumber(1,-1).angle() << "\n";
+	cout << ComplexNumber(5,2).angle() << "\n";
+	cout << "-----------------------------------\n";
+	
+	cout << (ComplexNumber(1,1) == ComplexNumber(1,2)) << "\n";
+	cout << (ComplexNumber(1,1) == 1) << "\n";
+	cout << (0 == ComplexNumber()) << "\n";
+}
